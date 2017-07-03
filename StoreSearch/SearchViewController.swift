81b530d43c1e092429b9d023471871c1259f0560
passt0r <dima3@ymail.com>
2017-07-03 19:19:45 +0300

    Start working on project

---
+++ StoreSearch/SearchViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SearchViewController.swift
+//  StoreSearch
+//
+//  Created by Dmytro Pasinchuk on 03.07.17.
+//  Copyright © 2017 Dmytro Pasinchuk. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
