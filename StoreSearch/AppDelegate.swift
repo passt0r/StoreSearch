bd53f0aba89e6d230cab2e9f83283c43deb84832
passt0r <dima3@ymail.com>
2017-07-04 00:52:50 +0300

    Customize search bar and add app icon

--- StoreSearch/AppDelegate.swift
+++ StoreSearch/AppDelegate.swift
@@ -10,12 +10,20 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    func customizeAppearance() {
+        let tintBarColor = UIColor(red: 20/255, green: 160/255, blue: 160/255, alpha: 1)
+        UISearchBar.appearance().barTintColor = tintBarColor
+        
+        window?.tintColor = UIColor(red: 10/255, green: 80/255, blue: 80/255, alpha: 1)
+    }
 
     var window: UIWindow?
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        customizeAppearance()
+        
         return true
     }
 
